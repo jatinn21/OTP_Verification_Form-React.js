3fca293837b0b66f392cedf15d8b632242490f84
Jatin Sharma <mca22.jatin.sharma@sunstone.edu.in>
2023-08-14 21:15:39 +0530

    changes

--- client/src/Components/UserForm.js
+++ client/src/Components/UserForm.js
@@ -55,7 +55,7 @@ class UserForm extends Component {
 
     const { name, email, mobileNumber, DOB, gender, address, intro, resume } =
       this.state;
-    const res = await fetch("https://job-forma-app.onrender.com/register", {
+    const res = await fetch("/register", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
